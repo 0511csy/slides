d46ef992d6b367d9ea136f2356d195c8c9d27c5a
csy <277376753@qq.com>
2019-02-24 18:59:39 +0800

    commit

---
+++ main.js
@@ -0,0 +1,81 @@
+let $buttons = $('#buttonWrapper button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
+
+makeFakeSlides()
+$slides.css({transform:'translateX(-920px)'})
+active($buttons.eq(current))
+bindEvents()
+$('#next').on('click', function(){
+    goToSlide(current+1)
+})
+$('#previous').on('click', function(){
+    goToSlide(current-1)
+})
+
+let timer = setInterval(function(){
+    goToSlide(current+1)
+},2000)
+$('.container').on('mouseenter', function(){
+    window.clearInterval(timer)
+}).on('mouseleave', function(){
+    timer = setInterval(function(){
+        goToSlide(current+1)
+    },2000)
+})
+
+function bindEvents(){
+    $('#buttonWrapper').on('click', 'button', function(e){
+        let $button = $(e.currentTarget)
+
+        let index = $button.index()
+        console.log(index);
+        active($buttons.eq(index-1))
+        goToSlide(index-1)
+    })
+}
+
+//重要
+function goToSlide(index){
+    if(index > $buttons.length-1){
+        index = 0
+    }else if(index <0){
+        index = $buttons.length - 1
+    }
+    console.log('current', 'index')
+    console.log(current, index)
+    if(current === $buttons.length -1 && index === 0){
+        // 最后一张到第一张,瞒天过海后来到第二张
+        console.log('here')
+        $slides.css({transform:`translateX(${-($buttons.length + 1) * 920}px)`})
+            .one('transitionend', function(){
+                $slides.hide()
+                $slides.offset()
+                $slides.css({transform:`translateX(${-(index+1)*920}px)`}).show()
+            })
+
+    }else if(current === 0 && index === $buttons.length - 1){
+        // 第一张到最后一张，瞒天过海后来到第二张
+        $slides.css({transform:`translateX(0px)`})
+            .one('transitionend', function(){
+                $slides.hide().offset()
+                $slides.css({transform:`translateX(${-(index+1)*920}px)`}).show()
+            })
+
+    }else{
+        $slides.css({transform:`translateX(${- (index+1) * 920}px)`})
+    }
+    current = index
+    active($buttons.eq(index))
+}
+function active($button) {
+    $button.addClass('act').siblings('.act').removeClass('act')
+}
+function makeFakeSlides(){
+    let $firstCopy = $images.eq(0).clone(true)
+    let $lastCopy = $images.eq($images.length-1).clone(true)
+
+    $slides.append($firstCopy)
+    $slides.prepend($lastCopy)
+}
